94e7037a9f14af768530c97d86aff9d707b7d55d
Pablo <juanpa92008@gmail.com>
2021-05-05 12:24:27 -0500

    Antes de crear Routers

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const AppRouter = () => {
+    return (
+        <div>
+            {/* Crear una ruta que tenga un path que apunte a /auth
+            NO es exacto
+            el componente que va a puntar es = a {AuthRouter}
+             */}
+
+             {/* 
+                apuntar con exact a path="/"
+                component = {JournalScreen}
+              */}
+        </div>
+    )
+}
---
+++ src/routers/AuthRouter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const AuthRouter = () => {
+    return (
+        <div>
+            {/* Crear rutas hijas a las cuales apuntará el AuthRouter 
+            AQUI NO VA ROUTER, SE COMIENZA CON SWITCH,
+            path="/auth/login"
+            component={LoginScreen}
+
+            path="/auth/register"
+            component={RegisterScreen}
+
+            al final, redirect a "/auth/login"
+             */}
+        </div>
+    )
+}
